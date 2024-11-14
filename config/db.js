8c3d1f47cad0295a6564cc454e4720c9c2789ab0
vishalgupta <vishalgupta91998@gmail.com>
2024-11-14 15:11:49 +0530

    added debugger

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-// config/db.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    const dbName = "HnFAssignment"
+  const dbName = "hnFAssignment";
   try {
-    console.log("try to connect ",process.env.MONGO_URI)
-    await mongoose.connect(`${process.env.MONGO_URI}/${dbName}`, {
+    // Construct the full URI by appending the dbName correctly
+    const uri = `${process.env.MONGO_URI}/${dbName}`;
+    console.log("Connecting to MongoDB:", uri);  // Optionally log the URI to verify
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
