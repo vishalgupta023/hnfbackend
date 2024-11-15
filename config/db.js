a5c65a9a37d1edf82e345bb059aaeef1ec3a66ae
vishalgupta <vishalgupta91998@gmail.com>
2024-11-15 10:40:39 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   const dbName = "hnFAssignment";
   try {
     // Construct the full URI by appending the dbName correctly
-    const uri = `${process.env.MONGO_URI}${dbName}`;
+    const uri = `${process.env.MONGO_URI}/${dbName}`;
     console.log("Connecting to MongoDB:", uri);  // Optionally log the URI to verify
     await mongoose.connect(uri, {
       useNewUrlParser: true,
